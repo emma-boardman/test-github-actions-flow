1dbda3245f81d9c846d97863cdaa9c8556bb2118
Emma Boardman <emma.boardman@shopify.com>
2023-03-23 17:00:17 +0000

    new change

--- packages/package-number-two/src/components/Button/Button.tsx
+++ packages/package-number-two/src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function Button(){
-    return <button type="button" id="1">Awesome button with sparkling new content</button>
+    return <button type="button" id="3">Awesome button with sparkling new content</button>
 }
