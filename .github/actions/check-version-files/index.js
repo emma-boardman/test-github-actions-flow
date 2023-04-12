1cf5a44dc13edaa510bc69ac66247a5b8e60f6b2
Emma Boardman <emma.boardman@shopify.com>
2023-04-12 11:51:47 +0100

    Add changeset check

---
+++ .github/actions/check-version-files/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const {getPackages} = require('@manypkg/get-packages');
+
+
+const main = async () => {
+  const cwd = process.cwd();
+
+  const {packages} = await getPackages(cwd);
+
+  const snapshotReleases = [];
+    
+  for (let index = 0; index < packages.length; index++) {
+      const {packageJson} = packages[index];
+
+      const pkgName = packageJson.name;
+      const localVersion = packageJson.version;
+
+      if (localVersion.includes('snapshot')){
+        snapshotReleases.push(`${pkgName}@${localVersion}`);
+      }
+    }
+
+    core.setOutput('SNAPSHOT_RELEASES', snapshotReleases);
+    core.setOutput('HAS_CHANGESET', snapshotReleases.length > 0);
+
+}
+
+main().catch((err) => core.setFailed(err.message));
