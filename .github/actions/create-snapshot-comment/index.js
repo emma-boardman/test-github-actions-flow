08b0ca4fd3a25e29c976174971a16422583d6463
Emma Boardman <emma.boardman@shopify.com>
2023-04-13 13:12:42 +0100

    improve comment

--- .github/actions/create-snapshot-comment/index.js
+++ .github/actions/create-snapshot-comment/index.js
@@ -9,24 +9,29 @@ const main = async () => {
 
   console.log('snapshots', typeof snapshots, snapshots);
 
-  // Snapshots are returned as a string "["snapshot", "snapshot"]""
-  // Revert to array format
-  snapshots = snapshots.replace(/"([^"]*)"/g, '$1').replace("[", "").replace("]","").split(',');
+  // Snapshots are returned as a string "["@shopify/package@0.0.0-snapshot-release-20230413115708", "@shopify/package@0.0.0-snapshot-release-20230413115708"]"
+  // Revert to an array:
+  // (1) Remove brackets
+  // (2) Remove double quotes
+  // (3) Create array
+  snapshots = snapshots.replace(/^\[|\]$/g, '').replace(/"/g, '').split(', '); 
 
-  const newTags = snapshots.map(([_, tag]) => tag)
-if (newTags.length) {
-  const multiple = newTags.length > 1
-  const body = (
-    `🫰✨ **Thanks @${github.context.actor}! ` +
-    `Your snapshot${multiple ? 's have' : ' has'} been published to [Cloudsmith](https://cloudsmith.io/~shopify/packages/?q=online-store-ui)\n\n` +
-    `Test the snapshot${multiple ? 's' : ''} by updating your \`package.json\` ` +
-    `with the newly published version${multiple ? 's' : ''}:\n` +
-    newTags.map(tag => (
-      '```sh\n' +
-      `yarn add ${tag}\n` +
-      '```'
-    )).join('\n')
-  )
+  if (snapshots.length) {
+    const snapshotMarkup =  snapshots.map(tag => (
+        '```sh\n' +
+        `yarn add ${tag}\n` +
+        '```'
+      )).join('\n');
+    const multiple = snapshots.length > 1
+    const body = (
+      `🫰✨ **Thanks @${github.context.actor}! ` +
+      `Your snapshot${multiple ? 's are' : ' is'} being published to [Cloudsmith](https://cloudsmith.io/~shopify/packages/?q=online-store-ui)\n\n` +
+      `Test the snapshot${multiple ? 's' : ''} by updating your \`package.json\` ` +
+      `with the newly published version${multiple ? 's' : ''}:\n` +
+       `${snapshotMarkup}` + `\n\n` +
+       `If you encounter any issues with your snapshots, look at the online-store-ui-snapshot-publish pipeline](link-when-it-exists) logs,` +
+       `or reach out in [#online-store-ui](https://shopify.slack.com/archives/CJL1EMP88)\n`
+    )
 
   const octokit = github.getOctokit(token);
 
