72d0bc84867f4e088136720475c357f42a8aae74
Emma Boardman <emma.boardman@shopify.com>
2023-04-13 12:27:45 +0100

    try file alternative

--- .github/actions/create-pr-comment/index.js
+++ .github/actions/create-pr-comment/index.js
@@ -5,38 +5,14 @@ const main = async () => {
     const token = core.getInput('GITHUB_TOKEN');
     const issue = core.getInput('ISSUE');
     const message = core.getInput('MESSAGE');
-    const reaction = core.getInput('REACTION');
-    // let snapshots = core.getInput('SNAPSHOTS');
-
-    // let comment = "Hi friend! Thanks for creating a snapshot. Test the snapshots by updating your `package.json` with the newly published versions";
-
-    // console.log(typeof snapshots);
-
-    // snapshots = snapshots.replace(/"([^"]*)"/g, '$1').replace("[", "").replace("]", "").split(",");
-
-    // snapshots.forEach(function (snapshot) {
-
-    //   comment += `\n \`\`\`${snapshot}\`\`\``;
-    // });
-
-    // console.log('issue', issue);
-    // console.log('snapshots', snapshots);
 
     const octokit = github.getOctokit(token);
 
-    const {data} = await octokit.rest.issues.createComment({
+    await octokit.rest.issues.createComment({
         issue_number: issue,
         body: message,
         ...github.context.repo,
       });
-
-      console.log('what data do I have', data);
-
-      // await octokit.rest.reactions.createForIssueComment({
-      //   comment_id: commentId,
-      //   content: reaction,
-      //   ...github.context.repo,
-      // });
 
 }
 
