34604cf04f227891c41b166d715d5f4c0d55a783
Emma Boardman <emma.boardman@shopify.com>
2023-04-14 11:33:48 +0100

    Add delete-snapshot-branch

--- .github/actions/delete-snapshot-branch/index.js
+++ .github/actions/delete-snapshot-branch/index.js
@@ -2,18 +2,44 @@ const github = require('@actions/github');
 
 const main = async () => {
     const token = core.getInput('GITHUB_TOKEN');
-    const branchRef = core.getInput('BRANCH_REF');
+    const featureBranch = core.getInput('FEATURE_BRANCH');
+    const snapshotBranchName = `snapshot-release/${featureBranch}`;
+    const snapshotBranchRef = `refs/heads/snapshot-release/${featureBranch}`;
+ 
     const octokit = github.getOctokit(token);
 
-    try {
-      await octokit.rest.git.deleteRef({
-          ref: branchRef,
-          ...github.context.repo,
-      })
-  } catch (error){
-      core.setFailed(`Reference could not be deleted ${error}` )
+
+    console.log('branchref',featureBranch);
+    console.log('snapshot', snapshotBranchName);
+    console.log('snapshotrer', snapshotBranchRef);
+
+   // Check if snapshot branch exists
+  try {
+    await octokit.rest.repos.getBranch({
+      ...github.context.repo,
+      branch: snapshotBranchName,
+    });
+
+    console.info("ℹ️ Snapshot branch found. Deleting...")
+
+  } catch (error) {
+    if (error.name === 'HttpError' && error.status === 404) {
+        throw Error("No Snapshot branch found. Exiting without deletion.");
+    } else {
+      throw Error(error);
+    }
   }
 
+  try {
+    // if branch exists, delete branch
+    await octokit.rest.git.deleteRef({
+      ref: snapshotBranchRef,
+      ...github.context.repo,
+    });
+
+  } catch (error) {
+    throw Error("Error deleting snapshot branch", error)
+  }
 }
 
 main().catch((err) => core.setFailed(err.message));
