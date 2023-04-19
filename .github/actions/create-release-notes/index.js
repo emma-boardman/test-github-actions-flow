229d457b00bc1e01156cf2f53a74bdf93a049f94
Emma Boardman <emma.boardman@shopify.com>
2023-04-19 15:32:50 +0100

    check result with multiple tags

--- .github/actions/create-release-notes/index.js
+++ .github/actions/create-release-notes/index.js
@@ -8,6 +8,8 @@ const main = async () => {
   const token = core.getInput('GITHUB_TOKEN');
   const tag = core.getInput('PUSHED_TAG').replace('refs/tags/', '');
 
+  console.log('what happens with multiple tags?', core.getInput('PUSHED_TAG'));
+
   const octokit = github.getOctokit(token);
 
   const [version, packageName] = tag.split('@');
