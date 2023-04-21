c3393d1906fe9cf5ae0e219efb1c7bd28ffd49ad
Emma Boardman <emma.boardman@shopify.com>
2023-04-21 17:40:16 +0100

    Add button

--- packages/package-number-two/src/components/Button/Button.tsx
+++ packages/package-number-two/src/components/Button/Button.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 export function Button(){
     return (<>
      <button type="button" id="1">Awesome button....</button>
-     <button type="button" id="1">Awesome button....</button>
-   
+
 
     </>
    )
