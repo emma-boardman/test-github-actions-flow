b30a279249b7713aacc5cf0c16930e14702050b3
Emma Boardman <emma.boardman@shopify.com>
2023-04-21 10:53:19 +0100

    pr to create VP pr

--- packages/package-number-two/src/components/Button/Button.tsx
+++ packages/package-number-two/src/components/Button/Button.tsx
@@ -17,6 +17,9 @@ export function Button(){
      <button type="button" id="1">Awesome button....</button>
      <button type="button" id="1">Awesome button....</button>
      <button type="button" id="1">Awesome button....</button>
+     <button type="button" id="1">Awesome button....</button>
+     <button type="button" id="1">Awesome button....</button>
+     <button type="button" id="1">Awesome button....</button>
 
     </>
    )
