f9e8cbc50797f7808e6afb769591c9cff52507a4
Emma Boardman <emma.boardman@shopify.com>
2023-04-21 15:42:48 +0100

    makes sense

--- .github/actions/create-update-release-pr-action/index.js
+++ .github/actions/create-update-release-pr-action/index.js
@@ -89,7 +89,7 @@ function getFileContentForCommit(versionFiles) {
     // Otherwise, capture local file changes for commit
     return {
       ...obj,
-      [name]: status.includes('D') ? '' : getFileContent(name),
+      [name]: status.includes('D') ? null : getFileContent(name),
     };
   }, {});
 
@@ -108,7 +108,6 @@ function getChangelogFileContent(fileName) {
   return fileContent.substring(newVersionIndex, lastVersionIndex);
 }
 
-
 function getPRDescription(versionFiles) {
   const introContent =
     "This PR was opened by the [OSUI Version Package](https://github.com/shopify/online-store-ui/.github/actions/changesets/close-existing-release-pr-action/action.yml) GitHub action. When you're ready to do a release, you can merge this and the packages will be published to npm automatically. If you're not ready to do a release yet, that's fine, whenever you add more changesets to main, a fresh Version Package PR will be created.";
@@ -125,7 +124,7 @@ function getPRDescription(versionFiles) {
     const {name} = fileDetails;
 
     const fileContent = getChangelogFileContent(name);
-    
+
     description += fileContent;
   });
   return description;
