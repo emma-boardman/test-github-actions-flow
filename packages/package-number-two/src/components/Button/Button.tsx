753722aff0b15c86e696c68928a3e22b540dee3c
Emma Boardman <emma.boardman@shopify.com>
2023-04-21 15:48:48 +0100

    button

--- packages/package-number-two/src/components/Button/Button.tsx
+++ packages/package-number-two/src/components/Button/Button.tsx
@@ -5,8 +5,6 @@ export function Button(){
      <button type="button" id="1">Awesome button....</button>
      <button type="button" id="1">Awesome button....</button>
      <button type="button" id="1">Awesome button....</button>
-
-
     </>
    )
 }
